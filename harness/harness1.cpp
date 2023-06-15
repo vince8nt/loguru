e40df565ffcfde2ff55528b28bc0ac5e916e0b0e
vince8nt <vince8nt@gmail.com>
2023-06-15 12:47:19 -0700

    use stdin

--- harness/harness1.cpp
+++ harness/harness1.cpp
@@ -8,11 +8,6 @@
 #define BUFF_SIZE 1024
 
 int main(int argc, char* argv[]) {
-    if (argc != 1)
-        return 0; // exit success so not counted as defect
-    int fd = open(argv[0], O_RDONLY);
-    if (fd < 0)
-        return 0; // exit success so not counted as defect
     char buff[BUFF_SIZE + 1];
     ssize_t bytesWritten = 0;
     ssize_t bytesRead = read(STDIN_FILENO, buff, BUFF_SIZE);
